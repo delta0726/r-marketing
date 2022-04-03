ee95519f7e4a056dc84a15d0beb8ed9dfdd996d1
Keido Sumi <delta0726@nta.pial.jp>
2022-04-04 08:25:40 +0900

    Update Chap09-main 多重共線性

--- book/marketing_research_analysis/ch09_線形モデルの発展トピック/chapter09-main_1_多重共線性.R
+++ book/marketing_research_analysis/ch09_線形モデルの発展トピック/chapter09-main_1_多重共線性.R
@@ -2,7 +2,6 @@
 # Title     : Rによる実践的マーケティングリサーチと分析
 # Chapter   : 9 線形モデルの発展的トピックス（メイン）
 # Objective : 多重共線性
-# Created by: Owner
 # Created on: 2021/02/22
 # Page      : P275 - P282
 # URL       : http://r-marketing.r-forge.r-project.org/Instructor/slides-index.html
@@ -10,9 +9,11 @@
 
 
 # ＜概要＞
-# - 多重共線性問題は最も認知された線形回帰モデルの落とし穴といえる
-# - ここでは｢変数除外｣と｢PCA変換｣について学ぶ
-#   --- 特徴量選択の機能を内包する正則化回帰がより実践的なソリューションといえる
+# - 多重共線性問題は最も認知された線形回帰モデルの落とし穴となっている
+# - ここでは多重共線性の認知方法と対処方法を学ぶ
+#   --- 認知方法 ：VIF
+#   --- 対処方法 ：変数除外 / PCA変換(主成分合成)
+#   --- 正則化回帰がより実践的なソリューションといえる（特徴量選択の機能）
 
 
 # ＜目次＞
@@ -28,6 +29,7 @@
 
 # ライブラリ
 library(tidyverse)
+library(magrittr)
 library(broom)
 library(psych)
 library(car)
@@ -48,27 +50,43 @@ cust.df %>% print()
 cust.df %>% glimpse()
 
 # サマリー
-cust.df %>% summary()
+cust.df %>% describe()
 
 
 # 1 最初の線形モデル ----------------------------------------------------------------------
 
 # ＜ポイント＞
-# - 何も考えずに元データを線形回帰モデルに適用
+# - 元データを基準化せずに線形回帰モデルに適用
 #   --- 線形回帰の前提を満たさないので、解釈しにくい結果が出てくる
 
-# モデル定義
-# --- 基準化もしていない状態
-spend.m1 <-
-   cust.df %>%
+
+# データ作成
+model_df <-
+  cust.df %>%
      select(-cust.id) %>%
-     filter(online.spend > 0) %>%
-     lm(online.spend ~ ., data = .)
+     filter(online.spend > 0)
+
+# モデル定義
+# --- 基準化をしていない状態で線形回帰モデルを定義
+spend.m1 <- lm(online.spend ~ ., data = model_df)
 
 # データ確認
-# --- 前処理していないので参考にならない
 spend.m1 %>% summary()
-spend.m1 %>% tidy()
+spend.m1 %>% listviewer::reactjson(collapsed = TRUE)
+
+# データ確認
+# --- 回帰係数
+# --- モデル統計量
+spend.m1 %>% summary()
+spend.m1 %>% glance()
+
+
+# 2 データ加工 ---------------------------------------------------------------------------
+
+# ＜ポイント＞
+# - データの外観を確認して必要な前処理を適用する
+#   --- Box-Cox変換：分布の正規化
+#   --- Zスコア変換：数値の水準/分散の調整
 
 
 # プロット作成
@@ -77,9 +95,6 @@ spend.m1 %>% tidy()
 cust.df %>%
   select_if(is.numeric) %>%
   scatterplotMatrix()
-
-
-# 2 データ加工 ---------------------------------------------------------------------------
 
 # 関数定義
 # --- Box-Cox変換してZスコア化
@@ -114,28 +129,35 @@ cust.df.bc %>%
 
 
 # モデル定義2
-spend.m2 <-
-  cust.df.bc %>%
-     lm(online.spend ~ ., data = .)
+# --- 前処理適用後のデータを使用
+spend.m2 <- lm(online.spend ~ ., data = cust.df.bc)
 
 # データ確認
 # --- online.transが圧倒的に有意
-spend.m2 %>% summary()
-spend.m2 %>% tidy()
+# --- 多くの回帰係数のp値が大きく「モデル間に違いがないという帰無仮説」を棄却できない
+spend.m2 %>% tidy() %>% mutate_if(is.numeric, round, 5)
+spend.m2 %>% glance()
+
+# VIFの確認
+# --- 10以上が複数で確認される（store.trans, store.spend）
+spend.m2 %>% vif() %>% round(2)
+
 
 # モデル定義3
 # --- online.transのみでモデル化
-spend.m3 <-
-  cust.df.bc %>%
-     lm(online.spend ~ online.trans, data = .)
+spend.m3 <- lm(online.spend ~ online.trans, data = cust.df.bc)
 
 # データ確認
-# --- online.transの傾きがほとんど1
-spend.m3 %>% summary()
-spend.m3 %>% tidy()
+# --- online.transの傾きがほとんど1（これだけで説明可能）
+spend.m3 %>% tidy() %>% mutate_if(is.numeric, round, 5)
+spend.m3 %>% glance()
+
 
 # 分散分析
-anova(spend.m3, spend.m2)
+anova_result <- spend.m3 %>% anova(spend.m2)
+anova_result %>% tidy()
+anova_result %>% glance()
+
 
 # VIFの確認
 # --- 10以上が複数で確認される
@@ -151,58 +173,71 @@ cust.df.bc %>%
 
 # 4 多重共線性への対処1：変数除外 -------------------------------------------------------------------
 
-# VIFの確認（再掲）
+# ＜ポイント＞
+# - データにおける多重共線性の程度はVIFにより判断することができる
+#   --- 単回帰の場合と比べて線形モデルの回帰係数の標準誤差がどれだけ増加するか
+#   --- VIFが10を超える変数を削除することで多重共線性を回避する
+
+
+# VIFの確認
+# --- VIFが10を超える変数を確認
 spend.m2 %>% vif()
+spend.m2 %>% vif() %>% .[. > 10] %>% names()
 
 # モデル定義4
-# --- VIFが10を超える変数を削除
-spend.m4 <-
-  cust.df.bc %>%
-     lm(online.spend ~ . -online.trans -store.trans, data = .)
+# --- VIFが10を超える2変数を削除
+spend.m4 <- lm(online.spend ~ . -online.trans -store.trans, data = cust.df.bc)
 
 # VIFの確認
 # --- 10以上がなくなった
 spend.m4 %>% vif()
 
 # データ確認
-spend.m4 %>% summary()
 spend.m4 %>% tidy()
+spend.m4 %>% glance()
 
 
 # 5 多重共線性への対処2：PCA変換 ------------------------------------------------------------------
 
-# solution 2: principal components
+# ＜ポイント＞
+# - 特徴量をPCA変換すると主成分ごとの相関係数がゼロとなるため多重共線性は生じない
+# - 元の特徴量を使わないので｢解釈面｣での利便性が損なわれる
+#   --- 対応策として類似した特徴量のみにPCAを適用することで合成変量に変換する
+
 
 # 相関係数の確認
-# --- 高相関であることを確認
+# --- 高相関ペアであることを確認（PCA合成の対象）
 cust.df.bc %>% select(online.visits, online.trans) %>% cor()
 cust.df.bc %>% select(store.trans, store.spend) %>% cor()
 
+# PCA合成
+# --- 変数の性質に応じて名前を付けている
+extract_pc1 <- function(df) df %>% prcomp() %>% use_series(x) %>% .[, 1]
+online <- cust.df.bc %>% select(online.visits, online.trans) %>% extract_pc1()
+store <- cust.df.bc %>% select(store.trans, store.spend) %>% extract_pc1()
+
 # データ加工
 # --- 高相関ペアのPC1を特徴量に追加
-# --- 変数の性質に応じて名前を付けている
 cust.df.bc.pc <-
   cust.df.bc %>%
-    mutate(online = prcomp(select(cust.df.bc, online.visits, online.trans))$x[ , 1],
-           store  = prcomp(select(cust.df.bc, store.trans, store.spend))$x[ , 1])
+    mutate(online = online,
+           store  = store) %>%
+    select(-online.visits, -online.trans, -store.trans, -store.spend)
 
 # データ確認
+# --- 高相関の変量がなくなっている
 cust.df.bc.pc %>% glimpse()
+cust.df.bc.pc %>% select(-online.spend) %>% select_if(is.numeric) %>% cor() %>% corPlot()
 
 # モデル定義4
 # --- 高相関ペアの説明変数を削除
 # --- PCAで合成した説明変数を追加
-spend.m5 <-
-  cust.df.bc.pc %>%
-     lm(online.spend ~ email + age + credit.score + distance.to.store +
-                      sat.service + sat.selection + online + store, data = .)
+spend.m5 <- lm(online.spend ~ ., data = cust.df.bc.pc)
 
 # VIFの確認
 # --- 最大で約1.5程度となった
 spend.m5 %>% vif()
 
 # データ確認
-# --- online.transの傾きがほとんど1
-spend.m5 %>% summary()
 spend.m5 %>% tidy()
-
+spend.m5 %>% glance()
