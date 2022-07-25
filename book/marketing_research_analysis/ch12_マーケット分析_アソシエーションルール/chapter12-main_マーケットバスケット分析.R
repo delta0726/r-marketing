9294bf59f504b6c9d40a7e824bfbf1e140700d07
Keido Sumi <delta0726@nta.pial.jp>
2022-07-26 08:07:36 +0900

    Update chap12

--- book/marketing_research_analysis/ch12_マーケット分析_アソシエーションルール/chapter12-main_マーケットバスケット分析.R
+++ book/marketing_research_analysis/ch12_マーケット分析_アソシエーションルール/chapter12-main_マーケットバスケット分析.R
@@ -2,10 +2,16 @@
 # Title     : Rによる実践的マーケティングリサーチと分析
 # Chapter   : 12 マーケットバスケット分析とアソシエーションルール
 # Theme     : 2 マーケットバスケット分析
-# Created on: 2021/04/12
+# Created on: 2022/07/26
 # Page      : P428 - P446
 # URL       : http://r-marketing.r-forge.r-project.org/Instructor/slides-index.html
 # ***********************************************************************************************
+
+
+# ＜概要＞
+# - 1では{arules}に含まれるスーパーマーケットのトランザクションの小規模データセットを使う
+#   --- データセットの構造は意識せず、分析プロセスと出力結果のイメージを確認する
+# - 2以降では実際のトランザクションデータを用いて分析を行う
 
 
 # ＜目次＞
@@ -16,8 +22,9 @@
 # 4 アソシエーションルールの検出
 # 5 ルールの可視化
 # 6 ルールをネットワークで可視化
-# 7 トランザクションデータの作成
-# 8 トランザクションと利益マージンデータを組み合わせた分析
+# 7 マージンデータの作成
+# 8 関数定義：マージンを調べる関数
+# 9 トランザクションと利益マージンデータを組み合わせた分析
 
 
 # 0 準備 ---------------------------------------------------------------------------------
@@ -34,14 +41,20 @@ conflict_prefer("some", "car", quiet = TRUE)
 
 
 # データロード
+# --- Groceries：transactionsという独自オブジェクトのサンプルデータ
+# --- retail.raw：行ごとに複数の購入商品が列挙されているテキストデータ
 data("Groceries")
 retail.raw <- readLines("data/retail.dat")
 
 
 # 1 サンプルデータでアソシエーション分析 ------------------------------------------------------
 
+# ＜ポイント＞
+# - サンプルデータであるGroceriesを用いてアソシエーション分析を行う
+
+
 # データ確認
-# --- 独自のオブジェクト
+# --- transactionsという独自オブジェクト
 # --- 9835個のトランザクションと169個のユニークアイテム
 Groceries %>% class()
 Groceries %>% summary()
@@ -51,11 +64,14 @@ Groceries %>% listviewer::reactjson(collapsed = TRUE)
 Groceries %>% head(3) %>% inspect()
 
 # アソシエーション分析
+# --- 支持度(supp)がトランザクションの1％以上
+# --- 確信度(conf)が0.3以上
 groc.rules <-
   Groceries %>%
     apriori(parameter = list(supp = 0.01, conf = 0.3, target = "rules"))
 
 # 確認
+groc.rules %>% print()
 groc.rules %>% summary()
 groc.rules %>% listviewer::reactjson(collapsed = TRUE)
 
@@ -66,7 +82,17 @@ groc.rules %>% subset(lift > 3) %>% inspect()
 
 # 2 スーパーマーケットデータの確認 -----------------------------------------------------------
 
+# ＜ポイント＞
+# - トランザクションデータは購買者ごとの購入商品の組み合わせのデータセット
+# - 以下のようなイメージ
+#   --- Aさん{石鹸、水、ガム}
+#   --- Bさん{クッキー、おむつ}
+#   --- Cさん{缶詰、刺身、ビール、食パン}
+
+
 # データ形式
+# --- データの塊ごとに""で区切られている(表示形式によっては改行されている)
+retail.raw %>% head()
 retail.raw %>% class()
 retail.raw %>% glimpse()
 
@@ -80,24 +106,42 @@ retail.raw %>% summary()
 
 # 3 データ加工と変換 ----------------------------------------------------------------------------
 
+# ＜ポイント＞
+# - apriori()を適用するためtransactionsオブジェクトを作成する
+#   --- 名前付きリストから作成
+
+
 # リスト変換
+# --- 取引ごとにリスト化して名前を付ける
 retail.list <-
-  strsplit(retail.raw, " ") %>%
+  retail.raw %>%
+    strsplit(" ") %>%
     set_names(str_c("Trans", seq_along(.)))
 
 # データ構造
 retail.list %>% glimpse()
 
 # データ確認
+# --- some()はランダムに指定数のサンプルを取得する
 retail.list %>% some()
 
 # データ変換
+# --- transactionsオブジェクトはリスト型から作成する
 retail.trans <- retail.list %>% as("transactions")
-retail.trans %>% summary()
 retail.trans %>% class()
+
+# サマリー
+# --- 88162 rows (elements/itemsets/transactions) and 16470 columns (items)
+# --- density of 0.0006257289 （ほとんどのアイテムはトランザクションで同時に買われていない）
+retail.trans %>% summary()
 
 
 # 4 アソシエーションルールの検出 ------------------------------------------------------------------
+
+# ＜ポイント＞
+# - transactionsオブジェクトからappriori()でルールを検出
+#   --- パイパーパラメータは支持度(supp)と確信度(conf)の2つ
+
 
 # モデル構築
 retail.rules <-
@@ -105,46 +149,73 @@ retail.rules <-
     apriori(parameter = list(supp = 0.001, conf = 0.4))
 
 # 確認
+# --- 支持度(supp)と確信度(conf)から5944のルールを検出
 retail.rules %>% summary()
+
+# オブジェクト構造
 retail.rules %>% listviewer::reactjson(collapsed = TRUE)
 
 
 # 5 ルールの可視化 -------------------------------------------------------------------------------
 
 # ＜ポイント＞
-# - ルール分布を理解する
+# - 支持度(support)と確信度(confidence)の2軸で散布図を作成する
+# - 固定チャートではインプリケーションが少ないが、インタラクティブだと得れる情報も多くなる
+
 
 # プロット作成
+# --- ほどんどのルールはあまり頻繁に発生しない（支持度が低い）
+# --- 確信度は比較的広範囲に分布している（0.4以上）
 retail.rules %>% plot()
 
 # プロット作成
 # --- インタラクティブ
-retail.rules %>% plot(interactive = TRUE)
+retail.rules %>% plot(interactive = TRUE, engine = "plotly")
 
 
 # 6 ルールをネットワークで可視化 ------------------------------------------------------------------
 
+# ＜ポイント＞
+# - ルール自体を可視化する際にはネットワークを用いる
+
+
 # ルールの抽出
+# --- マーケットバスケット分析の目標は高いリフト値を持つルールを検出すること
+# --- 単純に大きな数値を抽出すれば確認できる
 retail.hi <- retail.rules %>% sort(by = "lift") %>% head(50)
 
 # アソシエーション確認
 retail.hi %>% inspect()
 
+# アソシエーションのテーブル表示
+retail.hi %>% inspectDT()
+
 # ルールの一部を可視化
+# --- ネットワークでルールを表示すると、より高いレベルでテーマやパターンを探すことができる
 retail.hi %>% plot(method = "graph", control = list(type = "items"))
 
 
-# 7 トランザクションデータの作成 ---------------------------------------------------------
+# 7 マージンデータの作成 ----------------------------------------------------------------
+
+# ＜ポイント＞
+# - ルールに対応する収益性を検証するためのデータセットを作成する
+#   --- マージンデータ
+
 
 # 全アイテムの取得
-retail.itemnames <- retail.trans %>% as( "list") %>% unlist() %>% unique() %>% sort()
+retail.itemnames <-
+  retail.trans %>%
+    as( "list") %>%
+    unlist() %>%
+    unique() %>%
+    sort()
 
 # データ確認
 retail.itemnames %>% head()
 retail.itemnames %>% tail()
 
 # マージンデータの作成
-# --- 正規乱数で生成
+# --- 全アイテムに対して正規乱数で生成
 set.seed(03870)
 retail.margin <-
   data.frame(margin = rnorm(length(retail.itemnames), mean = 0.30, sd = 0.30)) %>%
@@ -166,11 +237,21 @@ retail.margin[basket.items,]
 sum(retail.margin[basket.items,])
 
 
-# 8 トランザクションと利益マージンデータを組み合わせた分析 ------------------------------------------
+# 8 関数定義：マージンを調べる関数 --------------------------------------------------------------
 
-retail.margsum <- function(items, itemMargins) {
-  # Input: "items" == item names, rules or transactions in arules format
-  #        "itemMargins", a data frame of profit margin indexed by name
+# ＜ポイント＞
+# -
+
+
+# ＜ステップ＞
+# - 1.与えられたデータをアイテム名集合のリストに変換
+# - 2.これらのアイテム名がマージンデータに含まれていることを確認
+# - 3.マージンを取り出して合計を計算
+
+
+retail.margsum <- function(itemMargins, items) {
+  # Input: "itemMargins", a data frame of profit margin indexed by name
+  #        "items" == item names, rules or transactions in arules format
   # Output: look up the item margins, and return the sum
   library(arules)
 
@@ -186,6 +267,7 @@ retail.margsum <- function(items, itemMargins) {
   } else {
     stop("Don't know how to handle margin for class ", class(items))
   }
+
   # make sure the items we found are all present in itemMargins
   good.items <- unlist(lapply(tmp.items, function(x)
     all(unlist(x) %in% rownames(itemMargins))))
@@ -201,12 +283,19 @@ retail.margsum <- function(items, itemMargins) {
   return(unlist(lapply(tmp.items, function(x) sum(itemMargins[x,]))))
 }
 
-retail.margsum(c("39", "48"), retail.margin)
-retail.margsum(list(t1 = c("39", "45"), t2 = c("31", "32")), retail.margin)
-retail.margsum(retail.trans[101:103], retail.margin)
-retail.margsum(retail.hi, retail.margin)
 
-retail.margsum(c("hello", "world"), retail.margin)  # error!
-retail.margsum(list(a = c("39", "45"), b = c("hello", "world"), c = c("31", "32")),
-               retail.margin)    # only the first and third are OK
+# 9 トランザクションと利益マージンデータを組み合わせた分析 ------------------------------------------
 
+# 実行例
+retail.margin %>% retail.margsum(items = c("39", "48"))
+retail.margin %>% retail.margsum(items = list(t1 = c("39", "45"), t2 = c("31", "32")))
+retail.margin %>% retail.margsum(items = retail.trans[101:103])
+retail.margin %>% retail.margsum(items = retail.hi)
+
+# エラー事例
+# retail.margin %>% retail.margsum(c("hello", "world"))  # error!
+
+# ワーニング事例
+# --- only the first and third are OK
+retail.margin %>%
+  retail.margsum(list(a = c("39", "45"), b = c("hello", "world"), c = c("31", "32")))
